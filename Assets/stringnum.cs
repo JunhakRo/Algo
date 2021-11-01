fe9b630b425597847dc15e776ab40af337287216
JunhakRo <88077002+JunhakRo@users.noreply.github.com>
2021-11-01 22:27:40 +0900

    문자열 내 마음대로 정렬하기

--- Assets/stringnum.cs
+++ Assets/stringnum.cs
@@ -2,41 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
-
-public class Algo : MonoBehaviour
+public class stringnum : MonoBehaviour
 {
+    //public string[] solution(string[] strings, int n)
+    //{
+    //    string[] answer = new string[] { };
 
-    // Start is called before the first frame update
-    void Start()
+    //    answer = strings.OrderBy(name => name).OrderBy(name => name[n]).ToArray();
+    //    return answer;
+    //}
+
+    public string[] solution()
     {
-
         string[] strings = { "abc", "cde", "efg", "ghi" };
         string[] answer = new string[strings.Length];
         int n = 1;
-        for (int i = 0; i < strings.Length; i++)
+       for(int i=0; i<strings.Length; i++)
         {
             answer[i] = strings[i].Substring(n, 1) + strings[i];
             print(i + ":" + answer[i]);
         }
         Array.Sort(answer);
-        
-        for(int i = 0; i<answer.Length; i++)
+
+        for (int i = 0; i < answer.Length; i++)
         {
             answer[i] = answer[i].Substring(1);
-            print(i+ "2:" + answer[i]);
+            print(i + "2:" + answer[i]);
 
 
         }
-       
-
+        return answer;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-            
-
-    }
 }
