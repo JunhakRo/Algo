b88b177eccb1939c808a73a0cebc823b5d103cbc
JunhakRo <88077002+JunhakRo@users.noreply.github.com>
2021-11-04 21:27:42 +0900

    이상한문자

--- Assets/esanghan.cs
+++ Assets/esanghan.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-
-public class Algo : MonoBehaviour
+public class esanghan : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
+    public string solution(string s)
     {
-        string s = "try hello world";
         int gongback = 0;
         string answer = "";
-        
+
         char[] ch = s.ToCharArray();
         for (int i = 0; i < ch.Length; i++)
         {
-            if(ch[i]==' ')
+            if (ch[i] == ' ')
             {
                 gongback = 0;
                 continue;
@@ -26,26 +22,16 @@ public class Algo : MonoBehaviour
             if (gongback % 2 == 0)
             {
                 ch[i] = Char.ToUpper(ch[i]);
-                print(i + ":" + s[i]);
             }
             else if (gongback % 2 != 0)
             {
                 ch[i] = Char.ToLower(ch[i]);
-                print(i + ":" + s[i]);
             }
 
             gongback++;
             answer = new string(ch);
 
         }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-            
-
+        return answer;
     }
 }
