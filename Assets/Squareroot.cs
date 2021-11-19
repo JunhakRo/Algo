0e5a9598d5d244c96063339cc9f43e7339312a97
JunhakRo <junhak1507@naver.com>
2021-11-19 16:20:43 +0900

    gg

---
+++ Assets/Squareroot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squareroot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 12;
+        print(x * *2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
