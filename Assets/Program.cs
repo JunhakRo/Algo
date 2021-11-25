57bffdb7811b7b56e2b6c88dd05baf9998d1e98e
JunhakRo <junhak1507@naver.com>
2021-11-25 16:39:03 +0900

    123123

---
+++ Assets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int answer = 1 ;
+            if (N == 0)
+            {
+                answer = 1;
+            }
+            else if (N != 0)
+            {
+                for (int i = 1; i <= N; i++)
+                {
+                    answer *= i;
+                    
+                }
+
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
