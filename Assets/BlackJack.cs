8a1171164ff531b8fc58169d7a9b02559c172346
JunhakRo <junhak1507@naver.com>
2021-11-30 17:15:21 +0900

    분해합... 수정중

--- Assets/BlackJack.cs
+++ Assets/BlackJack.cs
@@ -7,17 +7,17 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             string value = Console.ReadLine();
-            int[] setData = GetData(value);
+            //int[] setData = GetData(value);
 
             // setdata[0] count card
             // setdata[1] need value
             value = Console.ReadLine();
 
             // 입력받은 데이터를 int 배열로 가공
-            int[] Data = GetData(value);
+            //int[] Data = GetData(value);
 
             // BlakJack 함수를 사용하여 결과 값 출력
-            Console.WriteLine(BlackJack(setData, Data));
+           // Console.WriteLine(BlackJack(setData, Data));
         }
     }
 }
---
+++ Assets/Blackjack/Blackjack/Decomposition/Decomposition/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Decomposition
+{
+    //분해합을 구하라 ;;
+    class Program
+    {
+
+        //216의 분해합을 198 이다 ...
+        static void Main(string[] args)
+        {
+            List<int> sumList = new List<int>();
+            string N_num = Console.ReadLine();
+            int N = int.Parse(N_num);
+            //나머지값 
+            int result = 0;
+            //나머지값의 합
+            int result_sum = 0;
+            //증가값 
+            int number = AddNum(N);
+
+            if (number < N)
+            {
+
+                number++;
+
+                int division = (int)number / 10;
+
+
+                //나머지의 합 구하는 공식
+                while (division > 0)
+                {
+
+                    result = N % 10;
+                    result_sum += result;
+
+
+                    N = division;
+                    division = (int)N / 10;
+
+
+                }
+                if (division == 0)
+                {
+                    result = N;
+                    result_sum += result;
+                }
+
+                if (result_sum + number == N)
+                {
+                    sumList.Add(number);
+                    Console.WriteLine(sumList[0]);
+                }
+
+            }
+        }
+
+
+
+    }
+}
