05cb4cecdea05ecab890ba5f8b5f1217bfcb3cec
JunhakRo <88077002+JunhakRo@users.noreply.github.com>
2021-12-14 23:20:03 +0900

    단어정렬,STACK

---
+++ Assets/Sort1/Sort1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+namespace Sort1
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] a = new string[n];
+            
+
+            for(int i= 0; i<n; i++)
+            {
+                a[i] = Console.ReadLine();
+            }
+
+           var b =  a.OrderBy(x => x.Length).ThenBy(x => x) ;
+
+            foreach(var data in b)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+        int compare(string a, string b)
+        {
+            if(a.Length<b.Length)
+            {
+                return 1;
+            }
+            else if(a.Length>b.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                int aSum = GetSum(a);
+                int bSum = GetSum(b);
+            }
+        }
+        int GetSum(string a)
+        {
+            int legnth = a.Length, sum = 0;
+            for(int i = 0; i<legnth; i++)
+            {
+                if(a[i] -'0' <=9 && a[i]-'0'>=0)
+                {
+                    sum += a[i] - '0';
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Assets/Stack/Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace stack
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(7);
+            s.Push(5);
+            s.Push(4);
+            s.Pop();
+            s.Push(6);
+            s.Pop();
+            while(s.Count!=0)
+            {
+                Console.WriteLine(s.Peek());
+                s.Pop();
+            }
+
+        }
+    }
+}
